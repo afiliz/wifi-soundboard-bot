240630154e7b55836c94dc14d43916a7f97b4f1e
Adam Filiz <filizadam@gmail.com>
2022-02-24 01:06:34 +0000

    Sound bot can now play clips

--- wifi_soundboard_bot.py
+++ wifi_soundboard_bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord import FFmpegPCMAudio
 import nacl
 from discord.ext import commands
 from stayin_alive import keep_alive
@@ -10,7 +11,7 @@ Wi-Fi Tree Discord Bot
 This bot is designed for users of the Discord server "The Wi-Fi Tree". The bot's function is to play short sound clips on command when in a voice channel. The bot will join the caller's voice channel, play the selected clip, and leave the voice channel.
 '''
 
-bot = commands.Bot(command_prefix='$')
+bot = commands.Bot(command_prefix='!')
 secret_token = os.environ['TOKEN']
 
 @bot.event
@@ -43,7 +44,17 @@ async def play(ctx):
   #   async with ctx.typing():
       
   # # await ctx.channel.send('Playing clip')
-  pass
+  voice_client = ctx.message.guild.voice_client
+
+  audio_source = FFmpegPCMAudio('watch_your_profanity.mp3')
+  if voice_client.is_connected() and not voice_client.is_playing():
+    voice_client.play(audio_source, after=None)
+  else:
+    if not voice_client.is_connected():
+      await ctx.send("Can't play audio, the bot is not connected to a voice channel.")
+    elif voice_client.is_playing():
+      await ctx.send("Bot is already playing a sound clip. Wait your turn!")
+  
 
 
 @bot.command()
